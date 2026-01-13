646d474655aa8050affc1d82cdf6d4907199032f
Aulia Naufal Nurhaqiqi <166360029+aulianaufal@users.noreply.github.com>
2026-01-13 15:57:43 +0700

    Initial commit

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ st.write('Masukkan nilai akademik siswa untuk menentukan hasil performa.')
 
 # Input numerik
 math = st.number_input('Math Score', min_value=0)
-reading = st.number_input('Reading Score', min_value=0)
-writing = st.number_input('Writing Score', min_value=0)
+reading = st.number_input('Reading Score', min_value=0, max_value=100)
+writing = st.number_input('Writing Score', min_value=0, max_value=100)
 
 if st.button('Result'):
     # Validasi saat submit
@@ -43,4 +43,5 @@ if st.button('Result'):
         st.info('📌 Fokus pada penguatan materi yang belum dikuasai dan jaga konsistensi belajar.')
     else:
         st.warning(f'Rata-rata nilai: {avg:.2f}. Siswa termasuk kategori **Perlu Ditingkatkan**.')
+
         st.info('📌 Berdasarkan hasil evaluasi, diperlukan pembelajaran pada topik yang belum dikuasai, disertai materi tambahan dan latihan terarah untuk meningkatkan pemahaman materi.')
